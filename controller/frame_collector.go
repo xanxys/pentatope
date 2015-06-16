4f020f5571d2396a6f158e3e09d4501549972e29
xanxys <xanxys@gmail.com>
2015-06-16 21:45:16 +0900

    Insert []frame sorting step to FrameCollector. #47

--- controller/frame_collector.go
+++ controller/frame_collector.go
@@ -11,28 +11,19 @@ import (
 
 type FrameCollector struct {
 	framerate float32
-	imageDir  string
 
 	blobs map[int][]byte
 }
 
 func NewFrameCollector(framerate float32) *FrameCollector {
-	// Prepare output directory.
-	imageDir, err := ioutil.TempDir("", "penc")
-	if err != nil {
-		log.Panicln("Failed to create a temporary image directory", err)
-	}
 	return &FrameCollector{
 		framerate: framerate,
-		imageDir:  imageDir,
 		blobs:     make(map[int][]byte),
 	}
 }
 
 func (collector *FrameCollector) AddFrame(frameIndex int, imageBlob []byte) {
 	collector.blobs[frameIndex] = imageBlob
-	imagePath := path.Join(collector.imageDir, fmt.Sprintf("frame-%06d.png", frameIndex))
-	ioutil.WriteFile(imagePath, imageBlob, 0777)
 }
 
 func (collector *FrameCollector) RetrieveFrames() [][]byte {
@@ -47,23 +38,34 @@ func (collector *FrameCollector) RetrieveFrames() [][]byte {
 }
 
 func (collector *FrameCollector) EncodeToMp4File(outputMp4File string) {
+	// Prepare output directory.
+	imageDir, err := ioutil.TempDir("", "penc")
+	if err != nil {
+		log.Panicln("Failed to create a temporary image directory", err)
+	}
+	defer os.RemoveAll(imageDir)
+
+	for frameIndex, frameBlob := range collector.RetrieveFrames() {
+		imagePath := path.Join(imageDir, fmt.Sprintf("frame-%06d.png", frameIndex))
+		ioutil.WriteFile(imagePath, frameBlob, 0777)
+	}
+
 	cmd := exec.Command(
 		"ffmpeg",
 		"-y", // Allow overwrite
 		"-framerate", fmt.Sprintf("%f", collector.framerate),
-		"-i", path.Join(collector.imageDir, "frame-%06d.png"),
+		"-i", path.Join(imageDir, "frame-%06d.png"),
 		"-pix_fmt", "yuv444p",
 		"-crf", "18", // visually lossless
 		"-c:v", "libx264",
 		"-loglevel", "warning",
 		"-r", fmt.Sprintf("%f", collector.framerate),
 		outputMp4File)
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		log.Println("Encoding failed with", err)
 	}
 }
 
 func (collector *FrameCollector) Clean() {
-	os.RemoveAll(collector.imageDir)
 }
