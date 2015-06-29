87e9081ad7e17cda3e9c8d0239682d8f4765a2ad
xanxys <xanxys@gmail.com>
2015-06-29 23:35:00 +0900

    Adopt temporally smooth tonemapping from construct. #47

--- controller/frame_collector.go
+++ controller/frame_collector.go
@@ -38,8 +38,14 @@ func (collector *FrameCollector) RetrieveFrames() []*HdrImage {
 
 // Tonemap the frames so that they will fit in [0, 255] range.
 func Tonemap(framerate float32, frames []*HdrImage) []*HdrImage {
+	// pupillary reflex takes about 250ms to complete.
+	// http://www.faa.gov/data_research/research/med_humanfacs/oamtechreports/1960s/media/AM65-25.pdf
+	reflexLatency := 0.25
+	blendRatio := 1 - math.Pow(0.1, 1.0/(float64(framerate)*reflexLatency))
+
 	dispGamma := 2.2
 	ldrFrames := make([]*HdrImage, 0)
+	prev_maxv := -1.0
 	for _, frame := range frames {
 		// Calculate 99%-ile max_v
 		vs := make([]float64, len(frame.Values))
@@ -47,11 +53,20 @@ func Tonemap(framerate float32, frames []*HdrImage) []*HdrImage {
 			vs[ix] = float64(v)
 		}
 		sort.Sort(sort.Float64Slice(vs))
-		max_v := float32(vs[int(float64(len(vs))*0.99)])
+		max_v := vs[int(float64(len(vs))*0.99)]
+		// Smoothly blend.
+		smooth_max_v := 0.0
+		if prev_maxv < 0 {
+			smooth_max_v = max_v
+		} else {
+			smooth_max_v = math.Exp((1-blendRatio)*math.Log(prev_maxv) + blendRatio*math.Log(max_v))
+		}
+		prev_maxv = smooth_max_v
+
 		// Apply scaling
 		values := make([]float32, len(frame.Values))
 		for ix, v := range frame.Values {
-			values[ix] = float32(math.Pow(float64(v/max_v), 1/dispGamma) * 255)
+			values[ix] = float32(math.Pow(float64(v)/smooth_max_v, 1/dispGamma) * 255)
 		}
 		ldrFrame := &HdrImage{
 			Width:  frame.Width,
